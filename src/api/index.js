a8f2afcdff36c081dea1d7151912e1f0701c7ecb
Jyothi-morla23 <jyothim2303@gmail.com>
2025-11-13 11:36:03 +0530

    test

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://todo-backend-yvxy.onrender.com/api/todos/';
+const API_URL = 'https://todo-backend-yvxy.onrender.com/api/todos';
 
 export const getTodos = async () => {
   try {
